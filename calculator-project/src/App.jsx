9b72a9705fa73c283973dbbc3bea128016c48a59
koloso-s <zarychkrystian@gmail.com>
2025-01-23 14:58:58 +0100

    fix styles and dot

--- calculator-project/src/App.jsx
+++ calculator-project/src/App.jsx
@@ -16,6 +16,11 @@ function reducer(state, {type,payload}) {
       return {};
     case actions.add_digit:
       if(payload.digit === "0" && state.currrentOperand === "0")return state;
+      if(payload.digit === "." && state.currrentOperand == null && state.previousOperand == null){
+        return{
+          currrentOperand: "0."
+        }
+      }
       if(payload.digit === "." && state.currrentOperand.includes("."))return state;
       return {
         ...state,
